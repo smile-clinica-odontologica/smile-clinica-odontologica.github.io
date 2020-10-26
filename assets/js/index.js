729fa2d2de72c376ae8af7508079f7eff7f0c490
VelterGarcias <veltj2@gmail.com>
2020-10-25 23:58:43 -0300

    ajustes no menu mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const btnOpen = document.querySelector('.btn-open');
 const btnClose = document.querySelector('.btn-close');
+const closeMenu = document.querySelectorAll('[data-close="menu"]');
 const menu = document.querySelector('.menu');
 
 const body = document.querySelector('body')
@@ -7,12 +8,22 @@ const body = document.querySelector('body')
 btnOpen.addEventListener('click', e => {
 
   menu.classList.add('open')
+  body.classList.add('open')
+
 
 })
 
 btnClose.addEventListener('click', e => {
 
   menu.classList.remove('open')
+  body.classList.remove('open')
 
+})
+
+closeMenu.forEach(el => {
+  el.addEventListener('click', e=> {
+      menu.classList.remove('open')
+      body.classList.remove('open');
+  })
 })
 
